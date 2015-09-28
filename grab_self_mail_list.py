e0d354cfd3af48abe7a6b07133ec8615f253eaa3
Bystroushaak <bystrousak@kitakitsune.org>
2015-09-28 23:04:50 +0200

    Some of the uids raises 500 HTTPError. I've added check for 5 in row.

--- grab_self_mail_list.py
+++ grab_self_mail_list.py
@@ -35,27 +35,27 @@ def get_message(uid):
     try:
         data = DOWN.download(URL % uid)
     except HTTPError as e:
-        if e.code == 404:
-            return None
-
-        raise StopIteration()
+        return None
 
     return json.loads(data)
 
 
 def all_messages(db):
-    for i in xrange(100000000):
-        if i + 1 not in db:
-            yield get_message(i + 1)
+    for i in xrange(1000000):
+        uid = i + 1
+        if uid not in db:
+            yield uid, get_message(uid)
 
 
 class ShelveDB(object):
     def __init__(self, fn="msg_db.shelve"):
         self.fn = fn
         self.key = "msgs"
+        self.missing_key = "missing"
 
         with shelver("msg_db.shelve") as db:
             self.msgs = db.get(self.key, {})
+            self.missing = db.get(self.missing_key, set())
 
     def __setitem__(self, key, item): 
         self.msgs[key] = item
@@ -66,25 +66,36 @@ class ShelveDB(object):
     def __getitem__(self, key): 
         return self.msgs[key]
 
+    def __contains__(self, item):
+        return (item in self.msgs) or (item in self.missing)
+
     def has_key(self, k):
         return self.msgs.has_key(k)
 
-    def __contains__(self, item):
-        return item in self.msgs
+    def add_missing(self, uid):
+        self.missing.update([uid])
 
     def save(self):
         with shelver("msg_db.shelve") as db:
             db[self.key] = self.msgs
+            db[self.missing_key] = self.missing
 
 
 # Main program ================================================================
 if __name__ == '__main__':
     db = ShelveDB()
 
-    for message in all_messages(db):
+    missing_cnt = 0
+    for uid, message in all_messages(db):
+        if missing_cnt >= 5:  # 5 missing messages in a row means end
+            break
+
         if not message:
+            missing_cnt += 1
+            # db.add_missing(uid)  # this will ban last messages once they appear
             continue
 
+        missing_cnt = 0  # row of missing messages broken
         uid = message["ygData"]["msgId"]
         db[uid] = message
 
