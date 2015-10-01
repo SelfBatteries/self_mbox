c68a304291412ad5939309d2a39b695c1ae84fca
Bystroushaak <bystrousak@kitakitsune.org>
2015-10-01 21:53:37 +0200

    Added ._self_parse_time().

--- serializer.py
+++ serializer.py
@@ -4,10 +4,12 @@
 # Interpreter version: python 2.7
 #
 # Imports =====================================================================
-# import html2text
+import time
 import mailbox
 from email.parser import Parser
+from dateutil.parser import parse as date_parse
 
+# import html2text
 from grab_self_mail_list import ShelveDB
 
 
@@ -30,10 +32,67 @@ class Message(object):
         self.timestamp = timestamp
         self.subject = subject
         self.raw_email = raw_email
+        self.parsed = Parser().parsestr(raw_email.encode("utf-8"))
 
         self._topic_id = None
         self._prev_in_topic = None
         self._next_in_topic = None
+
+        self._postprocess()
+
+    def _postprocess(self):
+        self._self_parse_time()
+
+    def _self_parse_time(self):
+        def parse_date(s):
+            try:
+                return date_parse(s)
+            except ValueError:
+                return None
+
+        # try to parse dates in `Received` headers, which may look like this:
+        # (qmail 43149 invoked from network); 5 Sep 2011 13:53:18 -0000 (...)
+        received = (
+            parse_date(val.split(";")[-1].strip().split("(")[0])
+            for key, val in self.parsed.items()
+            if key == "Received"
+        )
+
+        def datetime_to_timestamp(dt):
+            """
+            Sometimes the year is out of range, so thats why the try..
+            """
+            try:
+                return time.mktime(dt.timetuple())
+            except ValueError:
+                return None
+
+        received_timestamps = [
+            datetime_to_timestamp(dt)
+            for dt in received
+            if dt  # to filter None from previous expression
+        ]
+
+        timestamps = received_timestamps
+        if "Date" in self.parsed:
+            date = parse_date(self.parsed["Date"])
+
+            if date:
+                timestamps.append(datetime_to_timestamp(date))
+
+        if self.timestamp > 0:
+            timestamps.append(self.timestamp)
+
+        if not timestamps:
+            raise ValueError("Couldn't find any reasonable timestamp!")
+
+        timestamp = min(
+            timestamp
+            for timestamp in timestamps
+            if timestamp and timestamp > 883612800  # skip < 1.1.1998
+        )
+
+        self.timestamp = int(timestamp)
 
     @staticmethod
     def from_json(j_msg):
@@ -44,7 +103,7 @@ class Message(object):
             author_name=j_msg["ygData"]["authorName"],
             subject=j_msg["ygData"]["subject"],
             raw_email=j_msg["ygData"]["rawEmail"],
-            timestamp=int(j_msg["ygData"].get("postDate", uid)),
+            timestamp=int(j_msg["ygData"].get("postDate", 0)),
         )
 
         msg._topic_id = j_msg["ygData"]["topicId"]
@@ -80,4 +139,9 @@ if __name__ == '__main__':
         for uid, msg in db.msgs.iteritems()
     }
 
-    print messages[max(messages.keys())]#, messages[-1].timestamp
+    last = messages[max(messages.keys())]
+    print last
+    print last.timestamp
+
+    # to_mbox(messages, "self")
+    print last.parsed["Date"]
